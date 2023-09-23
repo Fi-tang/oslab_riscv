8192b2182448c786e3cd5c929dac190b5c8e238b
kseiv <1102646562@qq.com>
2023-09-23 08:54:59 +0000

    put taskinfo at image:0x70

--- Project1-BootLoader/tools/createimage.c
+++ Project1-BootLoader/tools/createimage.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+#define EI_NIDENT (16)
+#define INT_IN_BYTES (4)
 #define IMAGE_FILE "./image"
 #define ARGS "[--extended] [--vm] <bootblock> <executable-file> ..."
 
@@ -14,6 +15,7 @@
 #define BOOT_LOADER_SIG_OFFSET 0x1fe
 #define OS_SIZE_LOC (BOOT_LOADER_SIG_OFFSET - 2)
 #define APP_NUMBER_LOC (BOOT_LOADER_SIG_OFFSET - 4)
+#define TASK_INFO_LOC 0x70
 #define BOOT_LOADER_SIG_1 0x55
 #define BOOT_LOADER_SIG_2 0xaa
 
@@ -21,7 +23,9 @@
 
 /* TODO: [p1-task4] design your own task_info_t */
 typedef struct {
-
+    char taskname[EI_NIDENT];
+    int start_block_id;
+    int total_block_num;
 } task_info_t;
 
 #define TASK_MAXNUM 16
@@ -93,7 +97,9 @@ static void create_image(int nfiles, char *files[])
     assert(img != NULL);
 
     /* for each input file */
-    long new_phyaddr_base = SECTOR_SIZE;
+    // long new_phyaddr_base = SECTOR_SIZE;
+    int section_counter = 0;
+    int task_id = 0;
     for (int fidx = 0; fidx < nfiles; ++fidx) {
 
         int taskidx = fidx - 2;
@@ -108,7 +114,6 @@ static void create_image(int nfiles, char *files[])
 
         /* for each program header */
         for (int ph = 0; ph < ehdr.e_phnum; ph++) {
-
             /* read program header */
             read_phdr(&phdr, fp, ph, ehdr);
 
@@ -116,13 +121,40 @@ static void create_image(int nfiles, char *files[])
 
             /* write segment to the image */
             write_segment(phdr, fp, img, &phyaddr);
-
+         
             /* update nbytes_kernel */
             if (strcmp(*files, "main") == 0) {
                 nbytes_kernel += get_filesz(phdr);
             }
-        }
 
+            // here, we padding but just place every application at the beginning
+            if(strcmp(*files, "bootblock") == 0){
+                section_counter = 1;
+                write_padding(img, &phyaddr, SECTOR_SIZE);
+            }
+            else{
+                // Adding sectors count
+                uint32_t current_filesz = get_filesz(phdr);
+                int current_file_sectors = NBYTES2SEC(current_filesz);
+                printf("***current_filesz: %d and that accounts for %d sectors\n", current_filesz, current_file_sectors);
+
+                section_counter += current_file_sectors;
+                // real section_id is section_counter - 1
+                // [p1-task4]: padding to new section
+                printf("***The section_counter = %d\n", section_counter);
+                int new_paddingRange = SECTOR_SIZE * section_counter;
+                write_padding(img, &phyaddr, new_paddingRange);
+            }
+            
+            // init task_info_t, task_id[0] = main
+            if(strcmp(*files, "bootblock") != 0){
+                strncpy(taskinfo[task_id].taskname, *files, strlen(*files));
+                taskinfo[task_id].total_block_num = NBYTES2SEC(get_filesz(phdr));
+                taskinfo[task_id].start_block_id = section_counter - NBYTES2SEC(get_filesz(phdr));
+                
+                task_id++;
+            }
+        }
         /* write padding bytes */
         /**
          * TODO:
@@ -130,24 +162,31 @@ static void create_image(int nfiles, char *files[])
          *  occupies the same number of sectors
          * 2. [p1-task4] only padding bootblock is allowed!
          */
-        if (strcmp(*files, "bootblock") == 0) {
-            write_padding(img, &phyaddr, SECTOR_SIZE);
-        }
-        else{
+        // if (strcmp(*files, "bootblock") == 0) {
+        //     write_padding(img, &phyaddr, SECTOR_SIZE);
+        // }
+        //else{
             // p1-task3, every application(including main), occupies 15 sectors
             // find error, the write_padding's new_phyaddr should be seriously calculated!
             // [p1-task3]: need addition for p1-task4
-            new_phyaddr_base += 15 * SECTOR_SIZE;
-            printf("  !!before padding :  This time's new_phyaddr_base = 0x%lx\n", new_phyaddr_base);
-            write_padding(img, &phyaddr, new_phyaddr_base);
-        }
+            // new_phyaddr_base += 15 * SECTOR_SIZE;
+            // printf("  !!before padding :  This time's new_phyaddr_base = 0x%lx\n", new_phyaddr_base);
+            // write_padding(img, &phyaddr, new_phyaddr_base);
+        //}
         fclose(fp);
         files++;
     }
-    printf("\n\n\ndebug... Entering write_img_info\n");
+    printf("\n\n\n***debug... Entering write_img_info\n");
     write_img_info(nbytes_kernel, taskinfo, tasknum, img);
 
     fclose(img);
+
+    // debug:
+    printf("\n***Printing taskinfo\n");
+    for(int i = 0; i < task_id; i++){
+        printf("**taskinfo[%d].taskname = %s\ttaskinfo[%d].start_block_id=%d\ttaskinfo[%d].total_block_num=%d\n",
+        i, taskinfo[i].taskname,  i,taskinfo[i].start_block_id,  i,taskinfo[i].total_block_num);
+    }
 }
 
 static void read_ehdr(Elf64_Ehdr * ehdr, FILE * fp)
@@ -238,15 +277,55 @@ static void write_img_info(int nbytes_kernel, task_info_t *taskinfo,
     ret_os_size = fwrite(&kernel_sectors, 2, 1, img);
     assert(ret_os_size == 1);
 
+    // [p1-task4], writing task_info_related message in 0x100
+    rewind(img);
+    fseek(img, TASK_INFO_LOC, SEEK_SET);
+    for(int i = 0; i < tasknum + 1; i++){
+        int app_info_result;
+        app_info_result = fwrite(&taskinfo[i], sizeof(task_info_t), 1, img);
+        assert(app_info_result == 1);
+    }
+
     // debug part A little revision, changing fopen(IMAGE_FILE, "w") to fopen(IMAGE_FILE, "w+")
     // the following can be negelected
+    // actually, we need 4 time's fgetc(img), this should be changed later, but currently, we only need < 256
     rewind(img);
     fseek(img, APP_NUMBER_LOC, SEEK_SET);
     printf("debug...:App_number = %d%d\n", fgetc(img), fgetc(img));
 
     rewind(img);
     fseek(img, OS_SIZE_LOC, SEEK_SET);
-    printf("debug...:os_total_size = %d%d\n", fgetc(img), fgetc(img));
+    printf("debug...:os_total_size = %d%d\n", fgetc(img), fgetc(img));  
+
+    rewind(img);
+    fseek(img, TASK_INFO_LOC, SEEK_SET);
+    // in memory:
+    // char tasknum occupies 16 byte, start_id occupies 4 byte, and num_of_blocks occupies 4 byte
+    for(int i = 0; i < tasknum + 1; i++){
+        task_info_t *temp = (task_info_t *)malloc(sizeof(task_info_t));
+        for(int j = 0; j < EI_NIDENT; j++){
+            temp -> taskname[j] = fgetc(img);
+        }
+        // the next number should be getting 4 fgetc()
+        int  real_start_block = 0;
+        int  start_block_count = 1;
+        for(int j = 0; j < INT_IN_BYTES; j++){
+            int base_number = (int)fgetc(img);
+            real_start_block = real_start_block + base_number * start_block_count;
+            start_block_count = start_block_count << INT_IN_BYTES;
+        }
+
+        int real_num_block = 0;
+        int num_block_count = 1;
+        for(int j = 0; j < INT_IN_BYTES; j++){
+            int base_number = (int)fgetc(img);
+            real_num_block = real_num_block + base_number * num_block_count;
+            num_block_count = num_block_count << INT_IN_BYTES;
+        }
+
+        printf("taskinfo[%d].taskname=%s\ttaskinfo[%d].start_block_id=%d\ttaskinfo[%d].num_block=%d\n",
+        i, temp -> taskname, i, real_start_block, i, real_num_block);
+    }
 }
 
 /* print an error message and exit */
